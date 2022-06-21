b8f45544df419b31a82ca96290cd04883bcf999e
Zoë Mackay <zoe.mackay@ovoenergy.com>
2022-06-21 08:24:29 +0100

    Small refactor

--- cloud_sql/cloud_sql.py
+++ cloud_sql/cloud_sql.py
@@ -26,102 +26,126 @@ def get_instance_from_nick(site: Site, nick: str, project: Optional[str]) -> Opt
         return None
 
 
+def print_list(site: Site, project: Optional[str]):
+    lines = site.print_list(project)
+    for line in lines:
+        print(line)
+
+
+def print_list_running(site: Site, running_instances: RunningInstances):
+    running = running_instances.get_all_running()
+    for connection_name in running.keys():
+        instance = site.instances[connection_name]
+        print(instance.print(running[connection_name]))
+    if len(running) == 0:
+        print("No running instances")
+
+
+def start(site: Site, running_instances: RunningInstances, name: str, project: Optional[str]):
+    if name == 'default':
+        instances = site.get_default_instances(project)
+        if len(instances) == 0:
+            print("No default instances found")
+    else:
+        instances = [get_instance_from_nick(site, name, project)]
+    for instance in instances:
+        if running_instances.get_running(instance.connection_name):
+            print(f"{instance.shortname} is already running.")
+        else:
+            pid = run_cloud_sql_proxy(config.cloud_sql_path, instance.connection_name, instance.port, instance.iam)
+            running_instances.add_running(pid, instance.connection_name)
+            print(f"Started {instance.name} on port {instance.port}")
+
+
+def stop(site: Site, running_instances: RunningInstances, nickname: str, project: Optional[str]):
+    if nickname == 'all':
+        instances = [site.instances[name] for name in running_instances.instances.keys() if
+                     (not project or project == site.instances[name].project)]
+        if len(instances) == 0:
+            print("No running instances found")
+    else:
+        instances = [get_instance_from_nick(site, nickname, project)]
+
+    for instance in instances:
+        pid = running_instances.get_running(instance.connection_name)
+        if pid:
+            if stop_cloud_sql_proxy(pid, instance.connection_name):
+                print(f"Stopped {instance.name} on port {instance.port}")
+            else:
+                print(
+                    f'Could not locate process to stop for {instance.shortname}, it has been removed from the running list')
+            running_instances.remove_running(instance.connection_name)
+        else:
+            print(f"{instance.shortname} is not running")
+
+
+def import_instances(site: Site, project: Optional[str]):
+    prev_instances = len(site.instances);
+    obtain_instances(site, project)
+    print(f"Imported {len(site.instances) - prev_instances} instances.")
+
+
+def update(site: Site, name: str, project: Optional[str], new_iam: Optional[str], new_nick: Optional[str],
+           new_default: Optional[str]):
+    instance = get_instance_from_nick(site, name, project)
+    if instance:
+
+        if new_iam:
+            instance.set_iam(new_iam.lower() == 'true')
+
+        if new_nick:
+            try:
+                other_instance = site.get_instance_by_nick_name(new_nick, instance.project)
+            except (InstanceNotFoundError, DuplicateInstanceError):
+                other_instance = None
+            if other_instance:
+                print('That nick would not be unique, pick another.')
+            else:
+                instance.shortname = new_nick
+                site.set_up_nicknames()
+
+        if new_default:
+            instance.set_default(new_default.lower() == 'true')
+
+        print('Instance updated:')
+        print(instance.print(None))
+
+
+def update_config(config: Configuration, new_path: Optional[str]):
+    if new_path:
+        try:
+            config.new_path(new_path)
+            print(f"Updated cloud_sql_proxy path to {new_path}")
+        except PathNotFoundError:
+            print("That file appears not to exist. It needs to be the fully qualified path.")
+    else:
+        print("Provide the location of the cloud_sql_proxy executable as parameter --path")
+
+
 def execute_command(parameters: Dict[str, str], config: Configuration, site: Site, running_instances: RunningInstances):
     command = parameters['command']
 
     if command == 'list':
-        lines = site.print_list(parameters['project'])
-        for line in lines:
-            print(line)
+        print_list(site, parameters['project'])
 
     elif command == 'list-running':
-        running = running_instances.get_all_running()
-        for connection_name in running.keys():
-            instance = site.instances[connection_name]
-            print(instance.print(running[connection_name]))
-        if len(running) == 0:
-            print("No running instances")
+        print_list_running(site, running_instances)
 
     elif command == 'start':
-        if parameters['name'] == 'default':
-            instances = site.get_default_instances(parameters['project'])
-            if len(instances) == 0:
-                print("No default instances found")
-        else:
-            instances = [get_instance_from_nick(site, parameters['name'], parameters['project'])]
-        for instance in instances:
-            if running_instances.get_running(instance.connection_name):
-                print(f"{instance.shortname} is already running.")
-            else:
-                pid = run_cloud_sql_proxy(config.cloud_sql_path, instance.connection_name, instance.port, instance.iam)
-                running_instances.add_running(pid, instance.connection_name)
-                print(f"Started {instance.name} on port {instance.port}")
+        start(site, running_instances, parameters['name'], parameters['project'])
 
     elif command == 'stop':
-        project = parameters['project']
-        nickname = parameters['name']
-        if nickname == 'all':
-            instances = [site.instances[name] for name in running_instances.instances.keys() if
-                         (not project or project == site.instances[name].project)]
-            if len(instances) == 0:
-                print("No running instances found")
-        else:
-            instances = [get_instance_from_nick(site, nickname, project)]
-
-        for instance in instances:
-            pid = running_instances.get_running(instance.connection_name)
-            if pid:
-                if stop_cloud_sql_proxy(pid, instance.connection_name):
-                    print(f"Stopped {instance.name} on port {instance.port}")
-                else:
-                    print(
-                        f'Could not locate process to stop for {instance.shortname}, it has been removed from the running list')
-                running_instances.remove_running(instance.connection_name)
-            else:
-                print(f"{instance.shortname} is not running")
+        stop(site, running_instances, parameters['name'], parameters['project'])
 
     elif command == 'update':
-        instance = get_instance_from_nick(site, parameters['name'], parameters['project'])
-        if instance:
-
-            new_iam = parameters['iam']
-            if new_iam:
-                instance.set_iam(new_iam.lower() == 'true')
-
-            new_nick = parameters['nick']
-            if new_nick:
-                try:
-                    other_instance = site.get_instance_by_nick_name(new_nick, instance.project)
-                except (InstanceNotFoundError, DuplicateInstanceError):
-                    other_instance = None
-                if other_instance:
-                    print('That nick would not be unique, pick another.')
-                else:
-                    instance.shortname = new_nick
-                    site.set_up_nicknames()
-
-            new_default = parameters['default']
-            if new_default:
-                instance.set_default(new_default.lower() == 'true')
-
-            print('Instance updated:')
-            print(instance.print(None))
+        update(site, parameters['name'], parameters['project'], parameters['iam'], parameters['nick'],
+               parameters['default'])
 
     elif command == 'import':
-        prev_instances = len(site.instances);
-        obtain_instances(site, parameters['project'])
-        print(f"Imported {len(site.instances) - prev_instances} instances.")
+        import_instances(site, parameters['project'])
 
     elif command == 'config':
-        new_path = parameters['path']
-        if new_path:
-            try:
-                config.new_path(new_path)
-                print(f"Updated cloud_sql_proxy path to {new_path}")
-            except PathNotFoundError:
-                print("That file appears not to exist. It needs to be the fully qualified path.")
-        else:
-            print("Provide the location of the cloud_sql_proxy executable as parameter --path")
+        update_config(config, parameters['path'])
 
     else:
         print("Specify a command or ask for help with --help")
